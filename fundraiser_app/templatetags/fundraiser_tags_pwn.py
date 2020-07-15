12803628e30b5cd95e5b4d207c809b96d4eba803
Kevin K <contact@cyberhavenprogramming.com>
2020-07-14 22:17:03 -0500

    added category_slug template filter
    Needed cause object level access is not available on queryset unless obj[index].attribute, which is sloppy.

--- fundraiser_app/templatetags/fundraiser_tags_pwn.py
+++ fundraiser_app/templatetags/fundraiser_tags_pwn.py
@@ -9,3 +9,8 @@ register = template.Library()  # necessary to be a valid tag library
 @register.filter  # can put (name='my_tag') next to filter for a diff name in template
 def markdown_format_fundraiser(text):
     return mark_safe(markdown.markdown(text))
+
+
+@register.filter
+def unslugify_and_capitalize(category_slug):
+    return category_slug.replace('-', ' ').title()
