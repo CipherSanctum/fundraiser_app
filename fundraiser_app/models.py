d1658010b2bb5e25bf3fced336b99d25b07d82ef
Kevin K <contact@cyberhavenprogramming.com>
2023-10-16 17:58:17 -0500

    Update models.py
    changed amount field with argument blank=False, thus requiring a positive value for form validation. Instead of default=0, which isn't  proper.

--- fundraiser_app/models.py
+++ fundraiser_app/models.py
@@ -88,7 +88,7 @@ class FundraiserDonation(models.Model):
     city = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    amount = models.DecimalField(max_digits=12, decimal_places=2, default=0)
+    amount = models.DecimalField(max_digits=12, decimal_places=2, blank=False)
     paid = models.BooleanField(default=False)
     trans_id = models.CharField(max_length=150, blank=True)
 
